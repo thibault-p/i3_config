b0f818b48cc6b320b2b4854ab5c335cabebebe8f
thibault-p <thibault.person@gmail.com>
2017-12-01 16:01:15 +0100

    Add fonts download

--- install.sh
+++ install.sh
@@ -2,11 +2,32 @@
 sudo apt install feh i3blocks zsh numlockx dunst
 
 
+
 # Retrieve script dir
 DIR="$( cd "$( dirname "$0" )" && pwd )"
+
+
+echo "Installing extra fonts..."
+PWD="$(pwd)"
+
+cd /tmp
+# clone
+git clone https://github.com/powerline/fonts.git --depth=1
+# install
+cd fonts
+./install.sh
+# clean-up a bit
+cd ..
+rm -rf fonts
+
+cd $PWD
 
 
 echo "Creating symlink..."
 ln -s $DIR/.i3/ $HOME/.i3
 ln -s $DIR/.i3blocks $HOME/.i3blocks
 ln -s $DIR/.wallpapers $HOME/.wallpapers
+ln -s $DIR/.oh-my-zsh/ $HOME/.oh-my-zsh
+
+echo "Setting zsh as default shell..."
+chsh -s /bin/zsh
